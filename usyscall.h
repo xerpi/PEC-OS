60d6cc39421133f466fa15974c8eccd78d5e19e2
Sergi Granell <xerpi.g.12@gmail.com>
2016-06-11 13:08:37 +0200

    Add missing usyscall.h

---
+++ usyscall.h
@@ -0,0 +1,32 @@
+#ifndef USYSCALL_H
+#define USYSCALL_H
+
+#include "libc.h"
+
+/* Syscalls ABI:
+ *   r0-r3: arguments
+ */
+
+static inline int syscall0_i(uint16_t number)
+{
+	__asm__(
+		"and r0, %0, %0\n\t"
+		"calls r0\n\t"
+		: : "r"(number)
+	);
+}
+
+static inline int syscall1_i(uint16_t number, uint16_t arg0)
+{
+	__asm__(
+		"and r0, %0, %0\n\t"
+		"and r1, %1, %1\n\t"
+		"calls r0\n\t"
+		: : "r"(number), "r"(arg0)
+	);
+}
+
+#define fork() ((int)syscall0_i(SYSCALL_FORK))
+#define getpid(pid) ((int)syscall0_i(SYSCALL_GETPID))
+
+#endif
